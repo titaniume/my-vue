5b7774c98695f3d2f009f83de9ce81532b53a2a8
titaniume <titaniume@163.com>
2020-05-24 16:47:47 +0800

    vuex基本用法

---
+++ vuex-demo1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   {{count}}
+   <button @click="$store.commit('increment')" >mutations---->count++</button>
+   <button @click="$store.dispatch('increment')">actions---->count++</button>
+   {{$store.getters.doubleCount}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  computed:{
+    count(){
+      return this.$store.state.count;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
