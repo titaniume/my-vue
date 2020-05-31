90b9ae0a2d4bbb968d0680de884557fadc9b6c94
titaniume <titaniume@163.com>
2020-05-31 21:42:07 +0800

    Vuex Router的使用场景

---
+++ router-demo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h2>router demo</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+</style>
